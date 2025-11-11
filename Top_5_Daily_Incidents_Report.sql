feff114f589e1ae70be635f9a7e2ec4d1d241ec1
raghadmohammedismail20-glitch <raghadmohammedismail20@gmail.com>
2025-11-11 21:22:10 +0200

    Update Top_5_Daily_Incidents_Report.sql
    Finalized portfolio query 1 - Top 5 daily incidents using CTEs and Window Functions

--- Top_5_Daily_Incidents_Report.sql
+++ Top_5_Daily_Incidents_Report.sql
@@ -1,1 +1,20 @@
+WITH RankedIncidents AS (
+SELECT 
+      date,
+      day_of_week,
+      incidnt_num,
+  ROW_NUMBER() OVER ( PARTITION BY day_of_week ORDER BY incidnt_num DESC ) AS day_rank 
+FROM tutorial.sf_crime_incidents_2014_01 )
+SELECT 
+     date, 
+     day_of_week,
+     incidnt_num,
+CASE 
+    WHEN day_of_week='Saturday' THEN 'Critical_Weekend_Incidents'
+    WHEN day_of_week='Sunday' THEN 'Critical_Weekend_Incidents'
+    ELSE 'Critical_Weekday_Incidents'
+END AS Incident_Type 
+FROM RankedIncidents
+WHERE day_rank <=5;
+
 
